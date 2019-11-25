998281f530e7b64647ee190d52e66c264eeb1d5b
James Garay <jmgaray11@gmail.com>
2019-11-25 13:54:49 -0500

    added a couple bigger test runs

--- src/main/java/com/jmg0/quakeanalysis/App.java
+++ src/main/java/com/jmg0/quakeanalysis/App.java
@@ -8,8 +8,17 @@ public class App {
 
     public static void main(String[] args) {
 
-        EarthquakeCollection earthquakeCollection = EarthquakeCollectionBuilder.build("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=2&starttime=2019-01-01&endtime=2019-01-02");
-        earthquakeCollection.printEarthquakeInfo();
+        // 84 quakes
+        //EarthquakeCollection earthquakeCollection = EarthquakeCollectionBuilder.build("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=2&starttime=2019-01-01&endtime=2019-01-02");
+        //earthquakeCollection.printEarthquakeInfo();
+
+        // 5513 quakes
+        //EarthquakeCollection earthquakeCollection2 = EarthquakeCollectionBuilder.build("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=2&starttime=2019-01-01&endtime=2019-03-01");
+        //earthquakeCollection2.printEarthquakeInfo();
+
+        // 13749 quakes
+        //EarthquakeCollection earthquakeCollection3 = EarthquakeCollectionBuilder.build("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=2&starttime=2019-01-01&endtime=2019-06-01");
+        //earthquakeCollection3.printEarthquakeInfo();
 
     }
 
